24085a101780a5c442fa2eef71e6b07c98d31f39
Jake <jake@jakestrang.com>
2025-05-26 13:48:45 -0400

    New: shallowEquals

--- src/compare.js
+++ src/compare.js
@@ -19,3 +19,20 @@ export function comparePath(a, b, path) {
     }
     return 0
 }
+
+// Returns true if the same, false if different
+export function shallowEquals(obj1, obj2) {
+    const keys1 = Object.keys(obj1)
+    const keys2 = Object.keys(obj2)
+
+    if (keys1.length !== keys2.length) {
+        return false
+    }
+
+    for (let key of keys1) {
+        if (!Object.hasOwn(obj2, key) || obj1[key] !== obj2[key]) {
+            return false
+        }
+    }
+    return true
+}
